bc4d658829577943ffcb9ebbf733ac73c5729fe0
khuxkm <khuxkm@tilde.team>
2020-09-06 04:45:36 +0000

    Make character info required for JSON output
    If we're being real, you need to supply character info for the JSON output, since the JSON to PNG compiler needs that information.

--- tojson.py
+++ tojson.py
@@ -6,16 +6,15 @@ import json
 
 parser = argparse.ArgumentParser(description="Reads DiscordScript files")
 parser.add_argument("script",type=argparse.FileType("r"),help="The script to read")
-parser.add_argument("-c","--characters",type=argparse.FileType("r"),help="A CSV file of the characters' script names, display names, and avatars.")
+parser.add_argument("charinfo",dest="characters",type=argparse.FileType("r"),help="A CSV file of the characters' script names, display names, and avatars.")
 parser.add_argument("-t","--time",help="The time to display on the clock thing. Will be consistent across all messages.")
 args = parser.parse_args()
 
 chars = dict()
-if args.characters:
-	r = csv.reader(args.characters)
-	for row in r:
-		if len(row)<2: continue
-		chars[row[0].upper()]=tuple(row[1:])
+r = csv.reader(args.characters)
+for row in r:
+	if len(row)<2: continue
+	chars[row[0].upper()]=tuple(row[1:])
 
 messages = dsp.parse(args.script)
 
